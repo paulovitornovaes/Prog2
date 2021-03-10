bc95d7aed9f5969ca7656c54f3e9b31e65b52a1d
Paulo Vitor <paulovitornovaes@id.uff.br>
2021-03-10 20:38:16 -0300

    finalizado primeira avaliacao

---
+++ lista_3/Lista3.Exercicio12.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+
+
+#define MAX 100
+
+void mostra(char *vet,int num);
+
+int main(){
+
+    int i,num = 0;
+    char vet[MAX], end = '\r';
+
+    printf("Sua frase vai ser printada:\n");
+
+    for (i = 0; i < MAX;i++)
+    {
+
+        vet[i] = getch();
+        printf("%c",vet[i]);
+        num++;
+        if (vet[i] == end)
+        {
+            break;
+        }
+
+    }
+    printf("Sua frase digitada sera´esta:\n");
+    //chamei a função montra com a duas variaveis//
+    mostra(vet,num);
+    return 0;
+
+}
+
+void mostra(char *vet,int num)
+{
+    int i;
+    for (i=0;i<num;i++)
+    {
+        if(vet[i] == ' '){
+            printf("\n");
+        }
+        printf("%c",vet[i]);
+
+    }
+
+}
---
+++ lista_4/ex01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *v;
+    v = (int *) malloc(10 * sizeof (int));
+
+}
